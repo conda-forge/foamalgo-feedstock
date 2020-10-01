6ec27fde0c56b8fd13ae40e748047b7479afd4e6
zhujun98 <zhujun981661@gmail.com>
2020-10-01 23:16:49 +0200

    Fix the python package import and c++ package run dependencies

--- recipe/build_cpp.sh
+++ recipe/build_cpp.sh
@@ -1,10 +1,8 @@
-$PYTHON -m pip install . --no-deps -vv
-
 mkdir build
 cd build
 cmake -DBUILD_FOAM_TESTS=OFF                  \
-      -DFOAM_USE_TBB=OFF                      \
-      -DFOAM_USE_XSIMD=OFF                    \
+      -DFOAM_USE_TBB=ON                       \
+      -DFOAM_USE_XSIMD=ON                     \
       -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR \
       -DCMAKE_INSTALL_LIBDIR=lib
 make install
